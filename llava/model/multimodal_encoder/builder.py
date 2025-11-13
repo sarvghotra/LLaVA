56f560a5bbed05658698993bd0dd151a7223348c
sarvghotra <sssarvjeet27@gmail.com>
2025-11-13 17:14:36 -0500

    Add clip checkpoint from local

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -3,7 +3,7 @@ from .clip_encoder import CLIPVisionTower
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
-    if vision_tower.startswith("openai") or vision_tower.startswith("laion"):
+    if vision_tower.startswith("openai") or vision_tower.startswith("laion") or vision_tower.endswith("clip-vit-large-patch14"):
         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
 
     raise ValueError(f'Unknown vision tower: {vision_tower}')
