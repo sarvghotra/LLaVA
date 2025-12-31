95f0963be14bbc2920102a3e6915f7dbd565060f
sarvghotra <sssarvjeet27@gmail.com>
2025-12-30 22:25:26 -0500

    Add open_clip vision tower support

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from transformers import CLIPVisionModel, CLIPImageProcessor, CLIPVisionConfig
+from open_clip import create_model_and_transforms
 
 
 class CLIPVisionTower(nn.Module):
@@ -67,6 +68,151 @@ class CLIPVisionTower(nn.Module):
     @property
     def device(self):
         return self.vision_tower.device
+
+    @property
+    def config(self):
+        if self.is_loaded:
+            return self.vision_tower.config
+        else:
+            return self.cfg_only
+
+    @property
+    def hidden_size(self):
+        return self.config.hidden_size
+
+    @property
+    def num_patches_per_side(self):
+        return self.config.image_size // self.config.patch_size
+
+    @property
+    def num_patches(self):
+        return (self.config.image_size // self.config.patch_size) ** 2
+
+
+class OpenCLIPVisionTower(nn.Module):
+    def __init__(self, vision_tower, args, delay_load=False):
+        super().__init__()
+        self.args = args
+
+        self.is_loaded = False
+
+        self.vision_tower_name = vision_tower
+        self.select_layer = args.mm_vision_select_layer
+        self.select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
+
+        if not delay_load:
+            self.load_model()
+        elif getattr(args, 'unfreeze_mm_vision_tower', False):
+            self.load_model()
+        else:
+            self.cfg_only = CLIPVisionConfig.from_pretrained(self.vision_tower_name)
+
+    def load_model(self, device_map=None):
+        if self.is_loaded:
+            print('{} is already loaded, `load_model` called again, skipping.'.format(self.vision_tower_name))
+            return
+
+        class ImageProcessorWrapper:
+            def __init__(self, img_processor):
+                self.img_processor = img_processor
+
+            def __call__(self):
+                return self.img_processor
+
+            def preprocess(self, image, return_tensors='.pt'):
+                enc = self.img_processor(image)
+                ret_load = {
+                    'pixel_values': [enc]
+                }
+                return ret_load
+
+
+        # self.image_processor = CLIPImageProcessor.from_pretrained(self.vision_tower_name)
+        # self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name, device_map=device_map)
+        self.vision_tower, image_processor = self.create_openclip_model_transform(self.vision_tower_name)
+        self.image_processor = ImageProcessorWrapper(image_processor)
+
+        self.vision_tower.requires_grad_(False)
+        self.is_loaded = True
+
+        # Add a shim config so your existing properties work
+        if not hasattr(self.vision_tower, 'config'):
+            class OpenCLIPConfig:
+                def __init__(self, model):
+                    self.hidden_size = model.visual.width   # FIXME (critical): adhoc way to match the shapes. Ideally, it should not be here.
+                    self.image_size = model.visual.image_size[0]
+                    self.patch_size = model.visual.patch_size[0]
+
+            self.vision_tower.config = OpenCLIPConfig(self.vision_tower)
+
+    def create_openclip_model_transform(self, model_name):
+        model, preprocess_train, preprocess_val = create_model_and_transforms(
+                "SEM_ViT-B-16",
+                "/home/mila/s/sarvjeet-singh.ghotra/scratch/git/open_clip/src/logs/SEM_ViT-B-32_laion4/checkpoints/epoch_7.pt",
+                precision="amp_bf16",
+                device=0,
+                jit=False,
+                force_quick_gelu=False,
+                force_custom_text=False,
+                force_patch_dropout=None,
+                force_image_size=None,
+                force_context_length=None,
+                image_mean=None,
+                image_std=None,
+                image_interpolation=None,
+                image_resize_mode=None,
+                aug_cfg={},
+                pretrained_image=False,
+                output_dict=True,
+                cache_dir=None,
+                strict_weight_load=True,
+                **{},
+            )
+
+        return model, preprocess_train
+
+    def feature_select(self, image_forward_outs):
+        image_features = image_forward_outs[self.select_layer + 1]
+        # FIXME: commented it to match the shapes with projector layer.
+        # if self.select_feature == 'patch':
+        #     image_features = image_features[:, 1:]
+        # elif self.select_feature == 'cls_patch':
+        #     image_features = image_features
+        # else:
+        #     raise ValueError(f'Unexpected select feature: {self.select_feature}')
+        return image_features
+
+    @torch.no_grad()
+    def forward(self, images):
+        if type(images) is list:
+            image_features = []
+            for image in images:
+                image_forward_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0))  # , output_hidden_states=True)
+                image_feature = self.feature_select(image_forward_out).to(image.dtype)
+                image_features.append(image_feature)
+        else:
+            image_forward_outs = self.vision_tower.visual.forward_intermediates(images.to(device=self.device, dtype=self.dtype), output_fmt='NLC')
+            # image_forward_outs = self.vision_tower.encode_image(images.to(device=self.device, dtype=self.dtype), normalize=False)  # , output_hidden_states=True)
+            image_forward_outs = image_forward_outs['image_intermediates']
+            image_features = self.feature_select(image_forward_outs).to(images.dtype)
+
+        return image_features
+
+    @property
+    def dummy_feature(self):
+        return torch.zeros(1, self.hidden_size, device=self.device, dtype=self.dtype)
+
+    @property
+    def dtype(self):
+        # return self.vision_tower.dtype
+        model_dtype = next(self.vision_tower.parameters()).dtype
+        return model_dtype
+
+    @property
+    def device(self):
+        # return self.vision_tower.device
+        model_device = next(self.vision_tower.parameters()).device
+        return model_device
 
     @property
     def config(self):
